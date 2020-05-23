8647d01074d293838600ecaca44528c8cb38054c
vamsi krishna <vamsichowdhury@gmail.com>
2020-05-23 21:00:13 +0530

    Updates

---
+++ precache-manifest.d483be3271d787604b9ab81e8cb4c76a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d30ef54afcdacfd00a7dfe84d4fc1b8",
+    "url": "/reacttodo/index.html"
+  },
+  {
+    "revision": "d2d1b62e643735588327",
+    "url": "/reacttodo/static/css/main.bcab33db.chunk.css"
+  },
+  {
+    "revision": "b29fa4a20fbafed5aaaf",
+    "url": "/reacttodo/static/js/2.4ce16388.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reacttodo/static/js/2.4ce16388.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2d1b62e643735588327",
+    "url": "/reacttodo/static/js/main.7c8075ee.chunk.js"
+  },
+  {
+    "revision": "22203c89df0fab7b0e21",
+    "url": "/reacttodo/static/js/runtime-main.adcb954a.js"
+  }
+]);
